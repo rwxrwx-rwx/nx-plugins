e47db238750adf266374cadf9bdc794e17b6101a
Kay - Khanh Bùi <khanhbui.lab@gmail.com>
2021-07-24 23:07:40 +0700

    chore(repo): apply yarn workspace for libs

---
+++ libs/nx-generators/lib/src/generators.js
@@ -0,0 +1,8 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.generators = void 0;
+function generators() {
+  return 'generators';
+}
+exports.generators = generators;
+//# sourceMappingURL=generators.js.map
---
+++ libs/nx-release/lib/src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const tslib_1 = require('tslib');
+tslib_1.__exportStar(require('./nx-release'), exports);
+// eslint-disable-next-line no-debugger
+debugger;
+//# sourceMappingURL=index.js.map
